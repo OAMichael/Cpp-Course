a03826cd893ac68a6582ceabcad65990be790348
OAMichael <mixa.ovsyann@mail.ru>
2021-10-11 18:40:08 +0300

    added README

---
+++ README.md
@@ -0,0 +1,1 @@
+This folder contains home tasks of C++ course
--- Triangles/TESTS/UnitTests/SwapPoints.cpp
+++ Triangles/TESTS/UnitTests/SwapPoints.cpp
@@ -7,11 +7,13 @@
 
 int main()
 {
-    Point A{0, 1, 2};
-    Point B{-1, 2, 1};
-    Point tmp{0, 0, 0};
-
-    SwapPoints(A, B);
-   // if ()
+    Geom::Point A{0, 1, 2};
+    Geom::Point B{-1, 2, 1};
+    
+    Geom::SwapPoints(A, B);
+    if (A == Geom::Point{-1, 2, 1} && B == Geom::Point{0, 1, 2})
+        std::cout << "Test 1:\t Passed" << std::endl;
+    else
+        std::cout << "Test 1:\t Failed" << std::endl;
     return 0;
 }
